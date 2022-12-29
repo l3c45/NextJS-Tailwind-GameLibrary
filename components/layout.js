ba9b5d15b2a13fc19da70f9b200cef4bd27b3c4e
lucas <mrlucaszarate@gmail.com>
2022-12-29 10:48:53 -0300

    Añadir Diseño de Lista de Juegos y APi REquest

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import SeachInput from "./seach-input";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-[#4a0339] flex items-center flex-col justify-between h-screen ">
+    <div className="bg-[#4a0339] flex items-center flex-col justify-between h-full ">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -14,12 +14,12 @@ const Layout = ({ children }) => {
           content="Learn how to build a personal website using Next.js"
         />
       </Head>
-      <header className="bg-zinc-900 relative w-full px-4 flex items-center justify-center h-1/5">
+      <header className="bg-zinc-900 relative w-full px-4 flex items-center justify-center h-24 ">
         <Image className=" absolute left-4 " src={logo} alt={"logo"} width={"100"}></Image>
      <h1 className="text-3xl text-fuchsia-600  text-center ">The Game Library</h1>
      <SeachInput ></SeachInput>
       </header>
-      <main className="w-full" >{children}</main>
+      <main className="w-full grid grid-cols-3 gap-4 p-10" >{children}</main>
       <footer className="w-full bg-zinc-900 text-center h-16 flex items-center justify-center" >
       <p className="text-xl text-fuchsia-600">l3c45</p>
       </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import Layout from '../components/layout'
-import SeachInput from '../components/seach-input'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({results}) {
+
+
   return (
     <>
      <Layout>
       <Head>
         <title>Games Library</title>
       </Head>
+
+      {results.map((game) => {
+        return (
+          <article className='flex flex-col items-center' key={game.id}>
+            <Image className='w-64 h-96 object-cover' width={700} height={500} src={game.background_image} alt="GAME IMAGE"></Image>
+            <h5 className='w-64 bg-zinc-900 text-center py-4 text-fuchsia-600 font-bold text-lg'>{game.name}</h5>
+          </article>
+        );
+      })}
       
      </Layout>
     </>
   )
 }
+
+
+
+export  const getServerSideProps= async ()=> {
+  const res=await fetch(`https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_APIKEY}`)
+ const games=await res.json()
+ const {results}=games
+  return {
+    props: {results}, 
+  }
+}
+
+
