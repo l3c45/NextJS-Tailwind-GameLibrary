a9feeba003d863922ea4a2504cf10fb939f8849b
lucas <mrlucaszarate@gmail.com>
2022-12-29 09:36:19 -0300

    Añadir diseño de header y footer en Layout

---
+++ components/layout.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import logo from "../public/logo.png";
+import SeachInput from "./seach-input";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="bg-[#4a0339] flex items-center flex-col justify-between h-screen ">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+      </Head>
+      <header className="bg-zinc-900 relative w-full px-4 flex items-center justify-center h-1/5">
+        <Image className=" absolute left-4 " src={logo} alt={"logo"} width={"100"}></Image>
+     <h1 className="text-3xl text-fuchsia-600  text-center ">The Game Library</h1>
+     <SeachInput ></SeachInput>
+      </header>
+      <main className="w-full" >{children}</main>
+      <footer className="w-full bg-zinc-900 text-center h-16 flex items-center justify-center" >
+      <p className="text-xl text-fuchsia-600">l3c45</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/seach-input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SeachInput = () => {
+  return (
+    <form className="absolute right-4">
+    <input className=' h-8 rounded-xl text-right focus:outline-none px-2'/>
+    <button></button>
+  </form>
+  )
+}
+
+export default SeachInput
