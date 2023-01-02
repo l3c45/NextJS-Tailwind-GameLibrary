381f5a55f5e5857229a9144d51eb08448d8c2318
lucas <mrlucaszarate@gmail.com>
2023-01-01 22:59:29 -0300

    Añadir Scroll Infinito

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,37 @@
-import '../styles/globals.css'
-import Router from 'next/router'
-import { useState, useEffect } from 'react';
-import Loader from '../components/loader';
-import NProgress from 'nprogress'
+import "../styles/globals.css";
+import Router from "next/router";
+import { useState, useEffect } from "react";
+import Loader from "../components/loader";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [queryClient] = useState(() => new QueryClient());
 
   useEffect(() => {
-    Router.events.on("routeChangeStart", (url)=>{
-      setIsLoading(true)
+    Router.events.on("routeChangeStart", (url) => {
+      setIsLoading(true);
     });
-    Router.events.on("routeChangeComplete", (url)=>{
-      setIsLoading(false)
+    Router.events.on("routeChangeComplete", (url) => {
+      setIsLoading(false);
     });
-  
-    Router.events.on("routeChangeError", (url) =>{
-      setIsLoading(false)
+
+    Router.events.on("routeChangeError", (url) => {
+      setIsLoading(false);
     });
-  
-  }, [Router])
-  
+  }, [Router]);
+
   return (
-  <>
-  {isLoading && <Loader/>}
-  <Component {...pageProps} />
-  </>)
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          {isLoading && <Loader />}
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,96 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import Layout from '../components/layout'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Layout from "../components/layout";
+import Link from "next/link";
+import { useQuery, dehydrate, QueryClient } from "react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
+import { useInfiniteQuery } from "react-query";
 
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Home({ dehydratedState }) {
+  const arr = dehydratedState.queries[0].state.data.results;
 
-export default function Home({necesaryData}) {
+  const router = useRouter();
 
- 
+  const [necesaryData, setNecesaryData] = useState(arr);
+  const [page, setPage] = useState(parseInt(router.query.page) || 1);
+  const { data } = useQuery(
+    ["characters", page],
+    async () =>
+      await fetch(
+        `https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_APIKEY}&page=${page}`
+      ).then((result) => result.json()),
+    {
+      keepPreviousData: true,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  );
 
-
-
+  async function handlePaginationChange() {
+    setPage((prev) => prev + 1);
+    const newData = await fetch(data.next);
+    const res = await newData.json();
+    setNecesaryData(necesaryData.concat(res.results));
+  }
 
   return (
-   
-     <Layout>
+    <Layout>
       <Head>
         <title>Games Library</title>
       </Head>
-<section className="w-full grid grid-cols-2  gap-1 md:grid-cols-4 md:gap-4 md:p-10 p-2">
-      {necesaryData.map((game) => {
-        return (
-          <article className='flex flex-col items-center my-2' key={game.id}>
-          <Link href={`game/${game.id}`}>
-            <Image className='w-40 h-64 md:w-64 md:h-96 object-cover ' width={700} height={500} src={game.bgImage} alt="GAME IMAGE"></Image>
-            
-            <h5 className=' w-40 md:w-64 bg-zinc-900 text-center py-4 text-fuchsia-600 font-bold text-lg'>{game.name}</h5>
-            </Link>
-          </article>
-       
-        );
-      })}
-      </section>
-      
-     </Layout>
-   
-  )
+
+      <InfiniteScroll
+        dataLength={necesaryData.length}
+        next={() => handlePaginationChange()}
+        hasMore={true}
+      >
+        <section className="w-full grid grid-cols-2  gap-1 md:grid-cols-4 md:gap-4 md:p-10 p-2">
+          {necesaryData.map((game, i) => {
+            return (
+              <article
+                className="flex flex-col items-center my-2"
+                key={`${game.id}${i}`}
+              >
+                <Link href={`game/${game.id}`}>
+                  <Image
+                    className="w-40 h-64 md:w-64 md:h-96 object-cover "
+                    width={700}
+                    height={500}
+                    src={game.background_image}
+                    alt="GAME IMAGE"
+                  ></Image>
+
+                  <h5 className=" w-40 md:w-64 bg-zinc-900 text-center py-4 text-fuchsia-600 font-bold text-lg">
+                    {game.name}
+                  </h5>
+                </Link>
+              </article>
+            );
+          })}
+        </section>
+      </InfiniteScroll>
+    </Layout>
+  );
 }
 
-
-
-export  const getServerSideProps= async ()=> {
-  const res=await fetch(`https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_APIKEY}`)
- const games=await res.json()
-
- const {results}=games
- const necesaryData=results.map(game=>{
-  return ({
-    name:game.name,
-    id:game.id,
-    bgImage:game.background_image
-  })
- })
-
- 
-  return {
-    props: {necesaryData}, 
+export async function getServerSideProps(context) {
+  let page = 1;
+  if (context.query.page) {
+    page = parseInt(context.query.page);
   }
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(
+    ["characters", page],
+    async () =>
+      await fetch(
+        `https://api.rawg.io/api/games?key=${process.env.NEXT_PUBLIC_APIKEY}&page=${page}`
+      ).then((result) => result.json())
+  );
+  return { props: { dehydratedState: dehydrate(queryClient) } };
 }
-
-
